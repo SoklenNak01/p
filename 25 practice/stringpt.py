6b89af2d83ae725e67e806ae6f5b89f587a0dbfb
unknown <Soklen.Nak@PNC.LAN>
2024-07-29 16:57:31 +0700

    yes

---
+++ 25 practice/stringpt.py
@@ -0,0 +1,21 @@
+# name1 = "Ronan"
+# name2 = "Channak"
+# name3 = "Seiha"
+# name4 = "Hugo"
+
+# output =" "
+# for i in range(4):
+#     name = input()
+#     nbofcha = 0
+#     for cha in name :
+#         nbofcha +=1
+#     output += str(nbofcha) + "-"
+# print(output[:-1])
+        
+# word = input()
+# n = len(word)
+# print(n*"y")
+
+# word = input()
+# for cha in word :
+#     print("y",end="")
